3ceb7426fb73c0e8168f703db31d98aa53d65ac0
korolkov_as <korolkovrussia@mail.ru>
2019-06-21 17:39:17 +0500

    finish

--- github/parspy_v1_random.py
+++ github/parspy_v1_random.py
@@ -1,53 +1,128 @@
-import requests, bs4, random, time
+import requests, bs4, random, time, psycopg2, re, uuid, urllib, os, urlparse
+from psycopg2 import sql
 
+#global
 s='https://mybook.ru'
 k='/catalog/biznes-knigi/books'
+file_dir='/home/artem/tmpjpg/'
 
 
-def call_web(s,k): # create html text url
-    print('call start')
-    time.sleep(0.5)
+def get_dml_db( values, insert_id, select_id, obj): # values=[('','', n)] insert_id= 1 or 2 select_id= 3 or 4, obj=path or url
+    x=''
+    case_sql = {
+        1: 'INSERT INTO CORE_TABLE (AVTOR, BOOK_NAME, PATH, COUNT_CITAT, URL) VALUES {}',
+        2: 'INSERT INTO CITATIONS (CITATION, ID_BOOKS) VALUES {}',
+        3: 'SELECT ID FROM CORE_TABLE WHERE PATH = %s',
+        4: 'SELECT ID FROM CORE_TABLE WHERE URL = %s'
+    }
+    print('call get_dml_db')
+    print('get_dml_db', insert_id, select_id, obj)
+    conn = psycopg2.connect(dbname='test_db', user='tpython',                   # connect db
+                            password='12345678', host='localhost', port='5432')
+    with conn.cursor() as cursor:
+        if insert_id > 0:
+            conn.autocommit = True
+            insert = sql.SQL(case_sql[insert_id]).format(
+                sql.SQL(',').join(map(sql.Literal, values))
+            )
+            cursor.execute(insert)
+            print('ok')
+        if select_id > 0:
+            cursor.execute(case_sql[select_id], (obj, ) )
+            for row in cursor:
+                x=row[0]
+    return x
+
+
+
+def get_call_web(s,k): # create html text url
+    print('call get_call_web')
+    print('get_call_web',s,k)
+    time.sleep(random.uniform(0.5,1.5))
     s=requests.get(s+k)
     return bs4.BeautifulSoup(s.text, 'html.parser')
 
-def search_max_page(b): # search max page
+
+def get_search_max_page(b): # search max page
+    print('call get_search_max_page')
     l=[]
     p=b.select('.ContextPagination__pagesButtons .PageButton__pageButton') # html current range page
     if len(p) > 0: # page > 0
         x=int(p[len(p)-1].getText().strip()) # end page
         for i in range(1,x+1):
             l.append(i)
+    else:
+        l.append(1)
     return l # [1, 2, 3, 4, ..., n] or []
 
-def random_page(x,f): # random page
-    global s, k
+
+def get_select(y):
+    global s, file_dir
+    #y='/author/l-e-kalinina/kommentarij-k-federalnomu-zakonu-o-privatizacii-go/' #test
+    a=[]
     p=[]
+    # select avtor and name book and name foto
+    b=get_call_web(s,y+'citations/') # call html pars
+    max=get_search_max_page(b)
+
+    for k in enumerate(b.findAll('a', attrs={'class':'BreadCrumbs__link'})): # avtor and book_name
+        a.append(k[1].text)
+    if len(a)==3: # check avtor, if NOT then a[3] = '-'
+        a.insert(3,a[2])
+        a.insert(2,'-')
+
+    d=b.select('.section .BookCitationListView__count')
+
+    for j in enumerate(b.findAll('link')): # select and save foto (jpg)
+        if re.search(r'c/200x300',j[1]['href']):
+            filename = str(uuid.uuid4())
+            file_path = urlparse.urlparse(j[1]['href']).path
+            ext = os.path.splitext(file_path)[1]
+            urllib.urlretrieve(re.sub(r'c/200x300','p/600x',j[1]['href']), file_dir+filename+ext)
+            print('filename', file_dir+filename+ext)
+            break
+
+    p.append((a[2],a[3],filename,re.match(r'\d*',d[0].text).group(),y)) # finish avtor and book name, count citats
+
+    id=get_dml_db(p,1,3,filename) # insert avtor and name book, select id book
+    for i in max:
+        n=[]
+        print('i=',i)
+        if i > 1:
+            b=get_call_web(s,y+'citations/'+'?page='+str(i))
+            print('next page')
+        for text in b.select('.TextTruncate__wrap .TextTruncate__text'):  # select citation
+            n.append((text.getText().strip(),id))
+        if len(n)>0:
+            get_dml_db(n,2,0,0)
+
+
+def get_random_page(x): # random page
+    print('call get_random_page')
+    global s, k
+
     if len(x) > 0:
         y=random.choice(x) # generate random page
         x.pop(x.index(y)) # remove index in list x
-        n=[div.a for div in call_web(s,k+'?page='+str(y)).findAll('div', attrs={'class' : 'ContextBookCard__bookTitle'})] # create list books
-        print('page',y,'im',len(x))
+        n=[div.a for div in get_call_web(s,k+'/?page='+str(y)).findAll('div', attrs={'class' : 'ContextBookCard__bookTitle'})] # create list books
+        print('get_random_page page=',y)
         for i in range(len(n)): # check random books
             y=random.choice(n)
             n.pop(n.index(y))
-            print('books',y['href'],'book',i,'in',len(n))
-            p=call_web(s,y['href']+'citations/').select('.TextTruncate__wrap .TextTruncate__text')
-            if len(p) >= 6: # if citations >= 6
-                f.append(y['href'])
-                print('f',len(f),'/3')
-                break
-    if len(f) == 3 and len(x) > 0:
-        return f
-    else:
-        return random_page(x,f)
+            print('get_random_page books=',y['href'])
+            if get_dml_db('',0,4,y['href']) == '':
+                get_select(y['href'])
+            else:
+                print('lose')
+            #break #test
+        get_random_page(x)  # if test then insert #
+        print('next page books')
+
+
+get_random_page(get_search_max_page(get_call_web(s,k)))
 
 
 
-#print(search_max_page(call_web(s,'/author/evgenij-shepin/vkusvill-kak-sovershit-revolyuciyu-v-ritejle-del-2/citations/')))
-
-print(random_page(search_max_page(call_web(s,k)),[]))
-
-#print(call_web(s,k).findAll('a', {'class': 'PageButton__pageButton'})['href'])
 
 
 
